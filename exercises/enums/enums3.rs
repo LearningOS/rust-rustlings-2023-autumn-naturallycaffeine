c4fefa09a03f4d38e1fb95265249f440622439ef
naturallycaffeine <113440427+naturallycaffeine@users.noreply.github.com>
2023-10-22 01:09:06 +0000

    init

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -5,11 +5,18 @@
 // Execute `rustlings hint enums3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 enum Message {
     // TODO: implement the message variant types based on their usage below
+    ChangeColor(u8, u8, u8),
+    Echo(String),
+    Move(Point),
+    Quit,
 }
+
+ 
+
 
 struct Point {
     x: u8,
@@ -39,10 +46,22 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
+        // 枚举类
+        match message {
+            Message::ChangeColor(r, g, b) => self.change_color((r, g, b)),
+            Message::Echo(s) => self.echo(s),
+            Message::Move(p) => self.move_position(p),
+            Message::Quit => self.quit(),
+        }
+
+
+// state.process(Message::ChangeColor((255, 0, 255)));
+
         // TODO: create a match expression to process the different message
         // variants
         // Remember: When passing a tuple as a function argument, you'll need
         // extra parentheses: fn function((t, u, p, l, e))
+       
     }
 }
 
@@ -57,7 +76,7 @@ mod tests {
             position: Point { x: 0, y: 0 },
             color: (0, 0, 0),
             message: "hello world".to_string(),
-        };
+                };
         state.process(Message::ChangeColor(255, 0, 255));
         state.process(Message::Echo(String::from("hello world")));
         state.process(Message::Move(Point { x: 10, y: 15 }));
